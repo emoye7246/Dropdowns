5945d08d127b1d86bf80e94a147efade6f6f98a5
Elijah <elijahmoye96@gmail.com>
2024-10-30 00:33:16 -0400

    Done WIth this
    Asssignment went well gonna start or maybe start
    to update the button styles but other than that
    it seems like we are ready to go

--- src/index.js
+++ src/index.js
@@ -2,13 +2,71 @@ import "/Users/elijahmoye/Desktop/dropdown/Dropdowns/src/css/styles.css";
 import _ from 'lodash'
 import { createImage } from "./java/carosel";
 import iced_Latte from '/Users/elijahmoye/Desktop/dropdown/Dropdowns/src/images/iced_Latte.jpg'
+import Mocha from '/Users/elijahmoye/Desktop/dropdown/Dropdowns/src/images/Mocha.jpg'
+import blackCoffee from '/Users/elijahmoye/Desktop/dropdown/Dropdowns/src/images/blackCoffee.jpeg'
+import Matcha from '/Users/elijahmoye/Desktop/dropdown/Dropdowns/src/images/Matcha.jpeg'
 
 
-let icedLatte = new createImage(iced_Latte, 'Iced Latte', 'Starbucks Drinks')
+let icedLatte = new createImage(
+    iced_Latte, 
+
+    'Name: Iced Latte', 
+
+    'Prices: Ranges from $3 to $10', 
+
+    'All you will need to make your own Iced Latte is Espresso,  Milk,  ice and sometimes simple syrup or vanilla', 
+
+    'Starbucks', 
+
+    'Made in 2009')
 icedLatte.createDisplay()
 
-let icedLattes = new createImage(iced_Latte, 'Iced Lattes', 'Starbucks')
-icedLattes.createDisplay()
+let icedMocha = new createImage(
 
-let icedLattess = new createImage(iced_Latte, 'Iced Lattes', 'Starbcks')
-icedLattess.createDisplay()
+    Mocha, 
+    'Iced Mocha', 
+
+    'Prices: Ranges from $3 to $12', 
+
+    'All you will need to make your own Iced Latte is Coffee, milk, Ice Cubes, chocolate syrup, and Sugar', 
+
+    'Starbucks', 
+
+    'According to Google this drink has been a flavorful expeirence since 1920')
+icedMocha.createDisplay()
+
+let blackCoffees = new createImage(
+    blackCoffee, 
+
+    'Black Cofffee', 
+
+    'Prices: Ranges from $1 to $5', 
+
+    '1 tableSpoon', 
+
+    'Starbucks', 
+
+    'Made in 2012')
+blackCoffees.createDisplay()
+
+let Matchas = new createImage(
+    Matcha, 
+
+    'Iced Matcha',
+
+    'Prices: Ranges from $3 to $10', 
+
+    '1 tableSpoon', 
+
+    'Starbucks', 
+    
+    'Made in 2010')
+Matchas.createDisplay()
+
+
+let icedMe = new createImage()
+icedMe.displayArray()
+
+
+
+
--- src/java/carosel.js
+++ src/java/carosel.js
@@ -5,11 +5,14 @@ let slides = 0
 
 export class createImage {
 
-    constructor(image, name, description){
+    constructor(image, name, description, ingredientsinfo, orderInfo, historyInfo){
 
         this.image = image, 
         this.name = name, 
-        this.description = description
+        this.description = description, 
+        this.ingredientsinfo = ingredientsinfo,
+        this.orderInfo = orderInfo, 
+        this.historyInfo = historyInfo
 
     }
 
@@ -21,24 +24,32 @@ export class createImage {
 
             name: this.name,
 
-            description: this.description
+            description: this.description, 
+
+            info: this.ingredientsinfo, 
+
+            order: this.orderInfo, 
+
+            history: this.historyInfo
+
         }
 
         images.push(pushMe)
                 
-
-        this.displayArray()
-
     }
 
     displayArray(){
 
         images.forEach((element) => {
 
+
+
             let corasel = document.getElementById('corasel')
     
             let store = document.createElement('div')
             store.id = 'drinks'
+
+            
     
             let myImage = document.createElement('img')
             myImage.src = `${element.image}`
@@ -52,14 +63,242 @@ export class createImage {
     
             let myDescription = document.createElement('div')
             myDescription.innerHTML = `${element.description}`
+
+            let countHolder = document.getElementById('countHolder')
+            
+
+            let count = document.createElement('div')
+            count.id = 'count'
+
+            
+
     
             store.append(myImage, myName, myDescription)
             corasel.append(store)
+            countHolder.append(count)
+         
+
+            store.addEventListener('click', () => {
+
+                this.displayDrink(element.image, element.name, element.description, element)
+            })
+
 
         })
+
+        this.scrollCommands()
 
 
     }
 
+    scrollCommands(){
+
+
+        let corasel = document.getElementById('corasel')
+        corasel.style.scrollBehavior = 'smooth'
+
+        let countHolder = document.getElementById('countHolder')
+        countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'red'
+
+
+
+
+        let test = function(){
+
+            countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'white'
+
+
+            if(slides < images.length - 1){
+
+            slides++
+            corasel.scrollLeft += 200
+            countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'red'
+
+            }
+            else if(slides == images.length - 1){
+
+                slides = 0
+                countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'red'
+
+                corasel.scrollTo(0, 0)
+            }
+            
+        }
+
+        setInterval(test, 5000)
+
+
+        let next = document.getElementById('next')
+        next.addEventListener('click', () => {
+            
+            corasel.scrollLeft += 200
+            test()
+            
+        })
+
+        let previous = document.getElementById('previous')
+        previous.addEventListener('click', () => {
+
+            if(slides == 0){
+
+            countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'red'
+
+            }
+            else if(slides != 0){
+            countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'white'
+            slides -= 1
+            corasel.scrollLeft -= 200
+            countHolder.querySelectorAll('div')[slides].style.backgroundColor = 'red'
+            }
+
+        })
+
+    }
+
+    displayDrink(photo, title, info, element){
+
+        let change = document.getElementById('change')
+        change.innerHTML = ' '
+        change.style.display = 'flex'
+
+        let countHolder = document.getElementById('countHolder')
+        countHolder.style.display = 'none'
+
+      let components = document.getElementById('components')
+      components.style.display = 'none'
+
+
+        let storeContent = document.createElement('div')
+        storeContent.id = 'storeContent'
+
+        let backButton = document.createElement('button')
+        backButton.id = 'backButton'
+        backButton.innerHTML = 'back'
+
+
+
+        let drinkPhoto = document.createElement('img')
+        drinkPhoto.src = `${photo}`
+        drinkPhoto.style.height = '200px'
+        drinkPhoto.style.width = '200px'
+
+        let wordContent = document.createElement('div')
+        wordContent.id = 'wordContent'
+
+
+        let drinkTitle = document.createElement('div')
+        drinkTitle.innerHTML = `${title}`
+
+        let drinkDescription = document.createElement('div')
+        drinkDescription.id = 'info'
+        drinkDescription.innerHTML = `${info}`
+
+        let myDropdown = document.createElement('div')
+        myDropdown.id = 'drop'
+
+
+
+        let learnMore = document.createElement('button')
+        learnMore.innerHTML = 'Learn More'
+
+        let dropdown = document.createElement('div')
+        dropdown.id = 'dropdown'
+        
+
+        let dropMe = false
+        let Drops = function(){
+            if(dropMe == false){
+
+                dropMe = true
+                dropdown.style.display = 'flex'
+            }
+            else if(dropMe == true){
+
+                dropMe = false
+                dropdown.style.display = 'none'
+            }
+        }
+
+
+        learnMore.addEventListener('click', () => {
+
+            Drops()
+         })
+
+
+        let moreInfo = document.createElement('div')
+        moreInfo.id = 'moreInfo'
+
+
+
+        let ingredients = document.createElement('div')
+        ingredients.innerHTML = 'Ingredients'
+
+        let order = document.createElement('div')
+        order.innerHTML = 'Order'
+
+        let history = document.createElement('div')
+        history.innerHTML = 'History'
+
+
+        
+        dropdown.append(ingredients, order, history)
+        myDropdown.append(learnMore, dropdown)
+        wordContent.append(drinkTitle, drinkDescription, myDropdown)
+        storeContent.append(backButton, drinkPhoto, wordContent)
+        change.append(storeContent, moreInfo)
+
+        ingredients.addEventListener('click', () => {
+
+            Drops()
+
+            moreInfo.innerHTML = ' '
+
+            let myIngredients = document.createElement('div')
+            myIngredients.innerHTML = `${element.info}`
+
+            moreInfo.appendChild(myIngredients)
+        })
+
+
+
+        order.addEventListener('click', () => {
+
+            Drops()
+
+
+            moreInfo.innerHTML = ' '
+
+            let myOrder = document.createElement('div')
+            myOrder.innerHTML = `${element.order}`
+
+            dropdown.style.display = 'none'
+
+            moreInfo.append(myOrder)
+        })
+
+        history.addEventListener('click', () => {
+            Drops()
+
+
+            moreInfo.innerHTML = ' '
+
+            let myHistory = document.createElement('div')
+            myHistory.innerHTML = `${element.history}`
+
+            moreInfo.append(myHistory)
+        })
+
+
+
+        backButton.addEventListener('click', () => {
+
+            change.style.display = 'none'
+           components.style.display = 'flex'
+           countHolder.style.display = 'flex'
+        })
+        
+        
+    }
 
 }
